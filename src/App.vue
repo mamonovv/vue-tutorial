b4ed889b2a948cee0cc8b73a570d3b52ee8ae2e7
Vlad Mamonov <mmvvlad@gmail.com>
2022-02-06 14:42:49 +0700

    Добавил пагинацию

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
+    <my-input v-model="searchQuery" placeholder="Поиск..." />
     <div class="app__btns">
       <my-button @click="showDialog">Создать пост</my-button>
       <my-select :options="sortOptions" v-model="selectedSort" />
@@ -11,10 +12,23 @@
     </my-dialog>
     <post-list
       v-if="!isPostsLoading"
-      :posts="sortedPosts"
+      :posts="sortedAndSearchedPosts"
       @remove="removePost"
     />
     <div v-else>Идет загрузка...</div>
+    <div class="page__wrapper">
+      <div
+        v-for="pageNumber in totalPages"
+        :key="pageNumber"
+        class="page"
+        :class="{
+          'current-page': page === pageNumber,
+        }"
+        @click="changePage(pageNumber)"
+      >
+        {{ pageNumber }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,7 +36,6 @@
 import PostForm from './components/PostForm.vue'
 import PostList from './components/PostList.vue'
 import axios from 'axios'
-import MySelect from './components/UI/MySelect.vue'
 
 export default {
   components: {
@@ -35,6 +48,10 @@ export default {
       dialogVisible: false,
       isPostsLoading: false,
       selectedSort: '',
+      searchQuery: '',
+      page: 1,
+      limit: 10,
+      totalPages: 0,
       sortOptions: [
         { value: 'title', name: 'По названию' },
         { value: 'body', name: 'По описанию' },
@@ -56,7 +73,16 @@ export default {
       try {
         this.isPostsLoading = true
         const response = await axios.get(
-          'https://jsonplaceholder.typicode.com/posts?_limit=10'
+          'https://jsonplaceholder.typicode.com/posts',
+          {
+            params: {
+              _page: this.page,
+              _limit: this.limit,
+            },
+          }
+        )
+        this.totalPages = Math.ceil(
+          response.headers['x-total-count'] / this.limit
         )
         this.posts = response.data
       } catch (e) {
@@ -64,6 +90,9 @@ export default {
       } finally {
         this.isPostsLoading = false
       }
+    },
+    changePage: function (pageNum) {
+      this.page = pageNum
     },
   },
   mounted() {
@@ -75,10 +104,15 @@ export default {
         return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
       })
     },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter((post) =>
+        post.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      )
+    },
   },
   watch: {
-    selectedSort(newValue) {
-      this.posts.sort()
+    page() {
+      this.fetchPosts()
     },
   },
 }
@@ -99,5 +133,19 @@ export default {
   margin: 15px 0;
   display: flex;
   justify-content: space-between;
+}
+
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+
+.current-page {
+  border: 2px solid teal;
 }
 </style>
--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="post">
     <div>
+      <div><strong>Номер поста: </strong> {{ post.id }}</div>
       <div><strong>Название: </strong> {{ post.title }}</div>
       <div><strong>Описание: </strong> {{ post.body }}</div>
     </div>
